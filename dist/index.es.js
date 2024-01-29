c32d0b3a83424d3fd2e4884680bbba40787ca5e0
Darwin <charmful0x@gmail.com>
2024-01-29 23:16:36 +0000

    feat: update dist to v1.0.6

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,11 +1,11 @@
-const i = "https://mem-cli-server-482a8c7c1299.herokuapp.com/mem-cli";
-function p(o, t) {
-  const n = o.split("").map((s) => s.charCodeAt(0)), e = {
+const c = "https://mem-cli-server-482a8c7c1299.herokuapp.com/mem-cli";
+function h(r, t) {
+  const n = r.split("").map((a) => a.charCodeAt(0)), s = {
     contractOwner: "",
     contentType: "application/javascript",
     contractSrc: n,
     initState: t
-  }, a = [
+  }, e = [
     { name: "Content-Type", value: "application/javascript" },
     { name: "Owner", value: "" },
     { name: "App-Name", value: "EM" },
@@ -13,54 +13,141 @@ function p(o, t) {
     { name: "EM-Bundled", value: "true" },
     { name: "Size", value: String(n.length) }
   ];
-  return { dataTx: e, tags: a };
+  return { dataTx: s, tags: e };
 }
-class u {
-  async readFunction(t) {
-    const n = `https://api.mem.tech/api/state/${t}`, a = await fetch(n, { headers: {
+async function p(r) {
+  var t;
+  try {
+    const n = r.split(".mem")[0], e = await fetch("https://api.mem.tech/api/state/GJn1h75nIAyMW5XWgzraL-Ldxr2Zb38WlLEVwk6CBDs", { headers: {
       "Content-Type": "application/json"
     } });
-    if (a.ok)
-      return await a.json();
-    throw new Error(a.statusText);
-  }
-  async writeFunction(t, n) {
-    const e = "https://api.mem.tech/api/transactions", c = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        functionId: t,
-        inputs: [
-          {
-            input: n
-          }
-        ]
-      })
-    }, r = await fetch(e, c);
-    if (r.ok)
-      return await r.json();
-    throw new Error(r.statusText);
-  }
-  async deployFunction(t, n) {
-    const e = JSON.stringify(p(t, n)), s = await fetch(i, {
-      method: "post",
-      url: i,
-      body: e,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      maxContentLength: 1e8,
-      maxBodyLength: 1e9
-    });
-    if (s.ok)
-      return { id: (await s.json()).txid };
-    throw new Error(s.statusText);
+    if (e.ok) {
+      const a = (t = await e.json()) == null ? void 0 : t.names;
+      if (n in a)
+        return a[n];
+    }
+    throw new Error(e.statusText);
+  } catch {
+    return null;
   }
 }
-class d extends u {
+class u {
+  constructor(t) {
+    this.network = t.network;
+  }
+  async readFunction(t) {
+    if (t.endsWith(".mem") && (t = await p(t)), this.network === "mainnet") {
+      const n = `https://api.mem.tech/api/state/${t}`, e = await fetch(n, { headers: {
+        "Content-Type": "application/json"
+      } });
+      if (e.ok)
+        return await e.json();
+      throw new Error(e.statusText);
+    }
+    if (this.network === "testnet") {
+      const n = `https://mem-testnet.xyz/state/${t}`, e = await fetch(n, { headers: {
+        "Content-Type": "application/json"
+      } });
+      if (e.ok)
+        return await e.json();
+      throw new Error(e.statusText);
+    }
+  }
+  async writeFunction(t, n) {
+    if (t.endsWith(".mem") && (t = await p(t)), this.network === "mainnet") {
+      const s = "https://api.mem.tech/api/transactions", o = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          functionId: t,
+          inputs: [
+            {
+              input: n
+            }
+          ]
+        })
+      }, i = await fetch(s, o);
+      if (i.ok)
+        return await i.json();
+      throw new Error(i.statusText);
+    }
+    if (this.network === "testnet") {
+      const s = "https://mem-testnet.xyz/write", a = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          function_id: t,
+          input: JSON.stringify(n)
+        })
+      }, o = await fetch(s, a);
+      if (o.ok)
+        return await o.json();
+      throw new Error(o.statusText);
+    }
+  }
+  async deployFunction(t, n) {
+    if (this.network === "mainnet") {
+      const s = JSON.stringify(h(t, n)), a = await fetch(c, {
+        method: "post",
+        url: c,
+        body: s,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        maxContentLength: 1e8,
+        maxBodyLength: 1e9
+      });
+      if (a.ok)
+        return { id: (await a.json()).txid };
+      throw new Error(a.statusText);
+    }
+    if (this.network === "testnet") {
+      const s = {
+        method: "post",
+        url: "https://mem-testnet.xyz/deploy",
+        body: JSON.stringify({
+          src: t,
+          state: n
+        }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        maxContentLength: 1e8,
+        maxBodyLength: 1e9
+      }, e = await fetch("https://mem-testnet.xyz/deploy", s);
+      if (e.ok)
+        return { id: (await e.json()).function_id };
+      throw new Error(e.statusText);
+    }
+  }
+  async forkFunction(t, n, s) {
+    if (this.network === "mainnet") {
+      const e = s ? `https://molext1.com/mem/fork/${t}/${n}/${s}` : `https://molext1.com/mem/fork/${t}/${n}`, o = await fetch(e, { headers: {
+        "Content-Type": "application/json"
+      } });
+      if (o.ok)
+        return await o.json();
+      throw new Error(o.statusText);
+    }
+  }
+  async kvFunction(t) {
+    if (this.network === "mainnet") {
+      const n = `https://mem-api.com/kv/${t}`, e = await fetch(n, { headers: {
+        "Content-Type": "application/json"
+      } });
+      if (e.ok)
+        return await e.json();
+      throw new Error(e.statusText);
+    }
+  }
+}
+class m extends u {
   async read(t) {
     return await this.readFunction(t);
   }
@@ -70,10 +157,16 @@ class d extends u {
   async deploy(t, n) {
     return await this.deployFunction(t, n);
   }
+  async fork(t, n, s) {
+    return await this.forkFunction(t, n, s);
+  }
+  async kvGet(t) {
+    return await this.kvFunction(t);
+  }
 }
-class h extends d {
+class d extends m {
 }
 export {
-  d as Actions,
-  h as Mem
+  m as Actions,
+  d as Mem
 };
